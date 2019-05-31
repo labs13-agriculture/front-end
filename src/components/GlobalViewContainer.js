c46c0a14ae00fd3375e37f24c3d837c89c97a979
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-05-31 14:13:08 -0500

    beginnings of a Global Side Nav

--- src/components/GlobalViewContainer.js
+++ src/components/GlobalViewContainer.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
 
-export default class GlobalViewContainer extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            names:[]
-        }
-    }
+export default class GlobalViewContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      names: []
+    };
+  }
 
-    render(){
-        return(
-            <div>
-                <h1>GlobalViewContainer</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <GVC>
+        <h1>Global View Container</h1>
+      </GVC>
+    );
+  }
 
-    // componentDidMount(){
-        
-    // }
+  // componentDidMount(){
+
+  // }
 }
 
+const GVC = styled.div`
+  display: flex;
+  width: 80%;
+  margin-bottom: -2000px;
+  padding-bottom: 2000px;
+  justify-content: center;
+`;

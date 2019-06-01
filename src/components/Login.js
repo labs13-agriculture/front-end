bb6d89d6960c1a9da09d463a3594ccd1e8e7ec7f
dgbailey <bailey.dust@gmail.com>
2019-05-31 19:22:20 -0700

    BUG:XOR logic to fix animation bug on production

--- src/components/Login.js
+++ src/components/Login.js
@@ -647,7 +647,7 @@ class Login extends Component {
             >
             <div
               className={`animation-div${
-                this.state.submitpw && !this.props.loginFailure ? " pulse" : ""
+                !this.state.submitpw != !this.props.loginFailure ? " pulse" : ""
               }`}
             />
 

fdc8cb2dcd7bbb7aa2e62a229226dbd4551a0cfd
AWSafran <andrewsafran@gmail.com>
2019-06-10 19:14:38 -0400

    implemented redux action and reducer for retailer search, currently listing names of all reatailers matching search criteria

--- src/components/RetailerSearchComponents/RetailerSearch.js
+++ src/components/RetailerSearchComponents/RetailerSearch.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import RetailerSearchForm from "./RetailerSearchForm";
+import { connect } from "react-redux";
+import { RetailerSearchResults as SearchAction } from "../../actions/RetailerSearch";
 
 class RetailerSearch extends Component{
     constructor(props){
@@ -7,15 +9,32 @@ class RetailerSearch extends Component{
     }
 
     submitSearch = query =>{
-        console.log(query);
+        this.props.SearchAction(query);
     }
 
 
     render(){
+        console.log(this.props.retailerData);
         return(
-            <RetailerSearchForm submitSearch={this.submitSearch}/>
+            <div>
+                <RetailerSearchForm submitSearch={this.submitSearch}/>
+                {this.props.searchSuccess && this.props.retailerData.map(r => <div>{r.name}</div>)}
+            </div>
         );
     }
 }
 
-export default RetailerSearch;
+const mapStateToProps = state =>{
+    return{
+        retailerData: state.retailerSearchData.data,
+        searchStart: state.retailerSearchData.searchStart,
+        searchFailure: state.retailerSearchData.searchFailure,
+        error: state.retailerSearchData.error,
+        searchSuccess: state.retailerSearchData.searchSuccess
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    { SearchAction }
+)(RetailerSearch);

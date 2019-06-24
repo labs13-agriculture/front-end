d2cf4e81660135de14e033043114e2da3e3e27af
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-06-24 13:56:52 -0500

    toggle modal on click out for add users

--- src/components/ManageUsers/SearchUsers.js
+++ src/components/ManageUsers/SearchUsers.js
@@ -1,98 +1,96 @@
 import React, { Component } from "react";
-import {UserResultsList} from "../ManageUsers/UserResultsList";
+import { UserResultsList } from "../ManageUsers/UserResultsList";
 import { connect } from "react-redux";
-import {userSearchResults} from '../../actions';
+import { userSearchResults } from "../../actions";
 import styled from "styled-components";
-import AddUser from './AddUser';
-import { Modal } from 'reactstrap';
-import {  media} from '../../styles/searchStyles';
+import AddUser from "./AddUser";
+import { Modal } from "reactstrap";
+import { media } from "../../styles/searchStyles";
 
- class SearchUsers extends Component{
-    constructor(props){
-        super(props);
+class SearchUsers extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            searchResults :[],
-            searchQuery:'',
-            toggleAddModal:false,
-            
-        }
+    this.state = {
+      searchResults: [],
+      searchQuery: "",
+      toggleAddModal: false
+    };
+  }
+
+  focusCursor() {
+    const field = document.querySelector(".search-input");
+
+    field.focus();
+  }
+
+  toggleAddModal = () =>
+    this.setState({ toggleAddModal: !this.state.toggleAddModal });
+
+  handleChanges = e => {
+    e.preventDefault();
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  componentDidMount() {
+    this.focusCursor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.props.userSearchResults(this.state.searchQuery);
     }
+  }
 
-    focusCursor() {
-        const field = document.querySelector(".search-input");
-    
-        field.focus();
-    }
-
-    
-    toggleAddModal = () => this.setState({toggleAddModal:!this.state.toggleAddModal})
-    
-    handleChanges = (e) =>{
-        e.preventDefault();
-        this.setState({searchQuery:e.target.value})
-        
-       
-    }
-
-   
-
-    componentDidMount(){
-        this.focusCursor();
-    }
-
-    componentDidUpdate (prevProps, prevState) {
-        if(prevState.searchQuery !== this.state.searchQuery) {
-          this.props.userSearchResults(this.state.searchQuery);
-        }
-      }
-
-    
-
-    render(){
-        return(
-            
-            <StyledSearchUsers>
-                <StyledSearchBar>
-                <div className="search-tools-cont">
-                <input placeholder="Search Username..." onChange={this.handleChanges} className="search-input" value={this.state.searchQuery}></input>
-                {/* <button onClick={()=>{this.props.userSearchResults(this.state.searchQuery)}} className="search-button">Search</button> */}
-                </div>
-                <div className="new-user-tools-cont">
-                <button onClick={this.toggleAddModal} className="search-button">ADD NEW</button>
-                </div>
-                <Modal isOpen={this.state.toggleAddModal}>
-            
-                    <AddUser userid={this.props.userid}/>
-                    <button onClick={this.toggleAddModal}  color="secondary">Cancel</button>
-                </Modal>
-                </StyledSearchBar>
-                <UserResultsList  returnedUserData={this.props.returnedUserData} userSearchSuccess={this.props.userSearchSuccess} userSearchStart={this.props.userSearchStart} />
-              
-            </StyledSearchUsers>
-        )
-    }
-
-
-
-
+  render() {
+    return (
+      <StyledSearchUsers>
+        <StyledSearchBar>
+          <div className="search-tools-cont">
+            <input
+              placeholder="Search Username..."
+              onChange={this.handleChanges}
+              className="search-input"
+              value={this.state.searchQuery}
+            />
+            {/* <button onClick={()=>{this.props.userSearchResults(this.state.searchQuery)}} className="search-button">Search</button> */}
+          </div>
+          <div className="new-user-tools-cont">
+            <button onClick={this.toggleAddModal} className="search-button">
+              ADD NEW
+            </button>
+          </div>
+          <Modal
+            isOpen={this.state.toggleAddModal}
+            toggle={this.toggleAddModal}
+          >
+            <AddUser
+              userid={this.props.userid}
+              toggleModal={this.toggleAddModal}
+            />
+            <button onClick={this.toggleAddModal} color="secondary">
+              Cancel
+            </button>
+          </Modal>
+        </StyledSearchBar>
+        <UserResultsList
+          returnedUserData={this.props.returnedUserData}
+          userSearchSuccess={this.props.userSearchSuccess}
+          userSearchStart={this.props.userSearchStart}
+        />
+      </StyledSearchUsers>
+    );
+  }
 }
-
 
 //begin styling
 
-  
-
 const StyledSearchUsers = styled.div`
-    height:100%;
-    display:flex;
-    flex-direction:column;
-    position:relative;
-
-    
-
-
-`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
 const StyledSearchBar = styled.div`
 
     width:100%;
@@ -165,17 +163,18 @@ const StyledSearchBar = styled.div`
 
         
 
-`
+`;
 
 const mapStateToProps = state => {
-   return{
-    returnedUserData:state.userReducer.data,
-    userSearchSuccess:state.userReducer.userSearchSuccess,
-    userSearchFailure:state.userReducer.userSearchFailure,
-    userSearchStart:state.userReducer.userSearchStart
+  return {
+    returnedUserData: state.userReducer.data,
+    userSearchSuccess: state.userReducer.userSearchSuccess,
+    userSearchFailure: state.userReducer.userSearchFailure,
+    userSearchStart: state.userReducer.userSearchStart
+  };
+};
 
-   }
-    
-}
-
-export default connect(mapStateToProps,{userSearchResults})(SearchUsers);
+export default connect(
+  mapStateToProps,
+  { userSearchResults }
+)(SearchUsers);

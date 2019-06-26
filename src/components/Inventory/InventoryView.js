6c9fca569be268a940f54015d27cf2bffe2b1b70
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-06-26 17:34:53 -0500

    success and failure messages for inventory add edit and delete

--- src/components/Inventory/InventoryView.js
+++ src/components/Inventory/InventoryView.js
@@ -2,59 +2,113 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 
-import InventoryTableHead from './InventoryTableHead';
-import InventoryItem from './InventoryItem';
-import AddItemModal from './AddItemModal';
+import InventoryTableHead from "./InventoryTableHead";
+import InventoryItem from "./InventoryItem";
+import AddItemModal from "./AddItemModal";
+import { clearInventoryAlerts } from "../../actions";
+import { Alert } from "reactstrap";
 
-import { theme } from '../../config'
+import { theme } from "../../config";
 
 import {
-    // Import actions here
-    getInventoryList, addItemToInventory, updateItemInInventory, deleteItemFromInventory
-} from '../../actions'
+  // Import actions here
+  getInventoryList,
+  addItemToInventory,
+  updateItemInInventory,
+  deleteItemFromInventory
+} from "../../actions";
 
 function InventoryView(props) {
+  // These are the stateful values
+  const [useModal, setModal] = useState(false);
 
-    // These are the stateful values
-    const [useModal, setModal] = useState(false);
-  
-    useEffect(() => {
-      // put in the call to get items here
-      // use effect gets called on every render,
-      // the 2nd argument is the stuff ti cares about, it wont run if that variable hasnt changed
-      // empty array only fires on mount
-        props.getInventoryList()
-    }, []);
-  
-    return (
-        <ViewContainer>
-            {/* Banner */}
-            <div className="banner">
-                <h2>Inventory</h2>
-            </div>
+  useEffect(() => {
+    // put in the call to get items here
+    // use effect gets called on every render,
+    // the 2nd argument is the stuff ti cares about, it wont run if that variable hasnt changed
+    // empty array only fires on mount
+    props.getInventoryList();
+  }, []);
 
-            <table className="inventory-table">
-                {/* Inventory card Container */}
-                <InventoryTableHead
-                    item={{id:"id", name:"name", quantity:"quantity", active:"active"}}
-                    doModal={() => {setModal(true)}}
-                />
-                <tbody>
-                {/* Inventory Cards */}
-                {props.inventoryList && props.inventoryList.map((i, index) => {
-                    return (
-                        <InventoryItem key={i.id} item={i} />
-                    );
-                })}
-                </tbody>
-            </table>
-            
-            {useModal && <AddItemModal doModal={() => setModal(false)}/>}
-        </ViewContainer>
-    );
+  const onDismiss = () => {
+    props.clearInventoryAlerts();
+  };
+
+  return (
+    <ViewContainer>
+      <div className="banner">
+        <h2>Inventory</h2>
+      </div>
+      <Alert
+        style={{ marginBottom: "0", width: "100%" }}
+        color="success"
+        isOpen={props.addSuccess}
+        toggle={onDismiss}
+      >
+        Add Success
+      </Alert>
+      <Alert
+        style={{ marginBottom: "0" }}
+        color="success"
+        isOpen={props.updateSuccess}
+        toggle={onDismiss}
+      >
+        Update Success
+      </Alert>
+      <Alert
+        style={{ marginBottom: "0" }}
+        color="success"
+        isOpen={props.deleteSuccess}
+        toggle={onDismiss}
+      >
+        Delete Success
+      </Alert>
+      <Alert
+        style={{ marginBottom: "0" }}
+        color="danger"
+        isOpen={props.addFailure}
+        toggle={onDismiss}
+      >
+        Failed To Add
+      </Alert>
+      <Alert
+        style={{ marginBottom: "0" }}
+        color="danger"
+        isOpen={props.updateFailure}
+        toggle={onDismiss}
+      >
+        Failed To Update
+      </Alert>
+      <Alert
+        style={{ marginBottom: "0" }}
+        color="danger"
+        isOpen={props.deleteFailure}
+        toggle={onDismiss}
+      >
+        Failed To Delete
+      </Alert>
+      <table className="inventory-table">
+        <InventoryTableHead
+          doModal={() => {
+            setModal(true);
+          }}
+        />
+        <tbody>
+          {/* Inventory Cards */}
+          {props.inventoryList &&
+            props.inventoryList.map((i, index) => {
+              return <InventoryItem key={i.id} item={i} />;
+            })}
+        </tbody>
+      </table>
+
+      {useModal && <AddItemModal doModal={() => setModal(false)} />}
+    </ViewContainer>
+  );
 }
 
-export default connect(state => ({
+export default connect(
+  state => ({
     //map state to props stuff here
     inventoryList: state.inventory.inventoryList,
     error: state.inventory.error,
@@ -62,43 +116,58 @@ export default connect(state => ({
     updatingInventory: state.inventory.updatingInventory,
     addingInventory: state.inventory.addingInventory,
     deletingInventory: state.inventory.deletingInventory,
-
-}), {
+    updateSuccess: state.inventory.updateSuccess,
+    updateFailure: state.inventory.updateFailure,
+    addSuccess: state.inventory.addSuccess,
+    addFailure: state.inventory.addFailure,
+    deleteSuccess: state.inventory.deleteSuccess,
+    deleteFailure: state.inventory.deleteFailure
+  }),
+  {
     // connect actions here
     getInventoryList,
     addItemToInventory,
     updateItemInInventory,
-    deleteItemFromInventory
-})(InventoryView)
-
+    deleteItemFromInventory,
+    clearInventoryAlerts
+  }
+)(InventoryView);
 
 const ViewContainer = styled.div`
-    height: 100%;
-    max-height: 100vh;
-    /* if you add width here it stretches container? */
-    background: #f3f3f3;
-    margin: 10px;
-    border-radius: 2px;
-    /* border-top: 5px solid #d3d3d3; */
+  height: 100%;
+  max-height: 100vh;
+  /* if you add width here it stretches container? */
+  background: #f3f3f3;
+  margin: 10px;
+  border-radius: 2px;
+  /* border-top: 5px solid #d3d3d3; */
 
-    overflow-y: scroll;
-    &::-webkit-scrollbar {
-        display: none;
-    }
-    
-    .banner {
-        /* border-bottom: 1.5px solid #d3d3d3; */
-        padding: 10px;
-        background: ${theme.navgrey};
-        color: ${theme.background_light};
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 
-        h2 {
-            font-size: 3rem;
-            font-weight: 700;
-        }
-    }
+  /* .alert {
+    background: ${theme.navgrey};
+    border: none;
+    border-radius: none !important;
 
-    .inventory-table {
-        width: 100%;
+    &.alert-danger {
+      color: palevioletred;
     }
+  } */
+  .banner {
+    padding: 10px;
+    background: ${theme.navgrey};
+    color: ${theme.background_light};
+
+    h2 {
+      font-size: 3rem;
+      font-weight: 700;
+    }
+  }
+
+  .inventory-table {
+    width: 100%;
+  }
 `;
--- src/reducers/inventoryReducer.js
+++ src/reducers/inventoryReducer.js
@@ -1,99 +1,140 @@
 import {
-    GETTING_INVENTORY, GETTING_INVENTORY_SUCCESS, GETTING_INVENTORY_FAILURE,
-    ADDING_INVENTORY, ADDING_INVENTORY_SUCCESS, ADDING_INVENTORY_FAILURE,
-    UPDATING_INVENTORY, UPDATING_INVENTORY_SUCCESS, UPDATING_INVENTORY_FAILURE,
-    DELETING_INVENTORY, DELETING_INVENTORY_SUCCESS, DELETING_INVENTORY_FAILURE
-
-} from "../actions"
+  GETTING_INVENTORY,
+  GETTING_INVENTORY_SUCCESS,
+  GETTING_INVENTORY_FAILURE,
+  ADDING_INVENTORY,
+  ADDING_INVENTORY_SUCCESS,
+  ADDING_INVENTORY_FAILURE,
+  UPDATING_INVENTORY,
+  UPDATING_INVENTORY_SUCCESS,
+  UPDATING_INVENTORY_FAILURE,
+  DELETING_INVENTORY,
+  DELETING_INVENTORY_SUCCESS,
+  DELETING_INVENTORY_FAILURE,
+  CLEAR_INVENTORY_ADD,
+  CLEAR_INVENTORY_UPDATE,
+  CLEAR_INVENTORY_DELETE
+} from "../actions";
 
 const initialState = {
-    inventoryList: [],
-    gettingInventory: false,
-    updatingInventory: false,
-    addingInventory: false,
-    deletingInventory: false,
-    error: false
-}
+  inventoryList: [],
+  gettingInventory: false,
+  updatingInventory: false,
+  addingInventory: false,
+  deletingInventory: false,
+  updateSuccess: false,
+  updateFailure: false,
+  addSuccess: false,
+  addFailure: false,
+  deleteSuccess: false,
+  deleteFailure: false,
+  error: false
+};
 
 export default (state = initialState, action) => {
-    switch(action.type){
-        case GETTING_INVENTORY:
-            return {
-                ...state,
-                error: false,
-                gettingInventory: true
-            }
-        case GETTING_INVENTORY_SUCCESS:
-            return {
-                ...state,
-                error: false,
-                gettingInventory: false,
-                inventoryList: action.payload
-            }
-        case GETTING_INVENTORY_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                gettingInventory: false
-            }
-        case UPDATING_INVENTORY:
-            return {
-                ...state,
-                error: false,
-                updatingInventory: true
-            }
-        case UPDATING_INVENTORY_SUCCESS:
-            return {
-                ...state,
-                error: false,
-                updatingInventory: false,
-                inventoryList: action.payload
-            }
-        case UPDATING_INVENTORY_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                updatingInventory: false
-            }
-        case ADDING_INVENTORY:
-            return {
-                ...state,
-                error: false,
-                addingInventory: true
-            }
-        case ADDING_INVENTORY_SUCCESS:
-            return {
-                ...state,
-                error: false,
-                addingInventory: false,
-                inventoryList: action.payload
-            }
-        case ADDING_INVENTORY_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                addingInventory: false
-            }
-        case DELETING_INVENTORY:
-            return {
-                ...state,
-                error: false,
-                deletingInventory: true
-            }
-        case DELETING_INVENTORY_SUCCESS:
-            return {
-                ...state,
-                error: false,
-                deletingInventory: false,
-                inventoryList: action.payload
-            }
-        case DELETING_INVENTORY_FAILURE:
-            return {
-                ...state,
-                error: action.payload,
-                deletingInventory: false
-            }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case GETTING_INVENTORY:
+      return {
+        ...state,
+        error: false,
+        gettingInventory: true
+      };
+    case GETTING_INVENTORY_SUCCESS:
+      return {
+        ...state,
+        error: false,
+        gettingInventory: false,
+        inventoryList: action.payload
+      };
+    case GETTING_INVENTORY_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        gettingInventory: false
+      };
+    case UPDATING_INVENTORY:
+      return {
+        ...state,
+        error: false,
+        updatingInventory: true
+      };
+    case UPDATING_INVENTORY_SUCCESS:
+      return {
+        ...state,
+        error: false,
+        updatingInventory: false,
+        updateSuccess: true,
+        inventoryList: action.payload
+      };
+    case UPDATING_INVENTORY_FAILURE:
+      return {
+        ...state,
+        updateFailure: true,
+        error: action.payload,
+        updatingInventory: false
+      };
+    case ADDING_INVENTORY:
+      return {
+        ...state,
+        error: false,
+        addingInventory: true
+      };
+    case ADDING_INVENTORY_SUCCESS:
+      return {
+        ...state,
+        error: false,
+        addingInventory: false,
+        addSuccess: true,
+        inventoryList: action.payload
+      };
+    case ADDING_INVENTORY_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        addingInventory: false,
+        addFailure: true
+      };
+    case DELETING_INVENTORY:
+      return {
+        ...state,
+        error: false,
+        deletingInventory: true
+      };
+    case DELETING_INVENTORY_SUCCESS:
+      return {
+        ...state,
+        error: false,
+        deletingInventory: false,
+        deleteSuccess: true,
+        inventoryList: action.payload
+      };
+    case DELETING_INVENTORY_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        deletingInventory: false,
+        deleteFailure: true
+      };
+
+    case CLEAR_INVENTORY_ADD:
+      return {
+        ...state,
+        addSuccess: false,
+        addFailure: false
+      };
+    case CLEAR_INVENTORY_UPDATE:
+      return {
+        ...state,
+        updateSuccess: false,
+        updateFailure: false
+      };
+    case CLEAR_INVENTORY_DELETE:
+      return {
+        ...state,
+        deleteSuccess: false,
+        deleteFailure: false
+      };
+    default:
+      return state;
+  }
+};

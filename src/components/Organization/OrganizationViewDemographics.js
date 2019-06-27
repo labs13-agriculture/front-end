bf0ae5dec0db184bc1dfbb6ca391286e00d3286a
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-06-26 22:19:40 -0500

    added semicolon

--- src/components/Organization/OrganizationViewDemographics.js
+++ src/components/Organization/OrganizationViewDemographics.js
@@ -168,7 +168,6 @@ const StyledDiv = styled.div`
   background: white;
   display: flex;
   flex-direction: column;
- 
 
   ${media.tablet`font-size: 1.2rem;`}
 
@@ -248,12 +247,12 @@ const StyledDiv = styled.div`
     }
   }
 
-  .spindiv{
-    width: 100%
+  .spindiv {
+    width: 100%;
     text-align: center;
   }
-  .spinner{
-    border: .5em solid lightgray;
+  .spinner {
+    border: 0.5em solid lightgray;
     border-right-color: transparent;
     width: 10rem;
     height: 10rem;
